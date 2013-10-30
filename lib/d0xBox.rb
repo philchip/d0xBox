b62c596bafd7e80e6fe46cc4a10a4c1542e81fb6
fewzy <bj@Benjamins-iMac.local>
2013-10-30 23:06:39 +0000

    modularising...

--- lib/d0xBox.rb
+++ lib/d0xBox.rb
@@ -5,92 +5,94 @@ require 'rubygems'
 require 'open-uri'
 require 'nokogiri'
 
-def bbb(s) # bit-by-bit printing
-	s.each_char { |c| putc c; sleep 0.025; STDOUT.flush }
-	print "\n\n"
-	sleep(2)
-end
 
-def search(p) # search text for a term
-	puts 'Yo. Enter search term: '
-	term = gets.chomp.downcase
-	count_results = p.downcase.scan(term).count
-	result_found = true
-	result_found = false if count_results == 0
+module D0xWeb
+	def self.bbb(s) # bit-by-bit printing
+		s.each_char { |c| putc c; sleep 0.025; STDOUT.flush }
+		print "\n\n"
+		sleep(2)
+	end
 
-	if result_found
-		print "Result found. Occurrences: #{count_results}\n"
-		print 'Search again? y/N: '
-		choice = gets.chomp.downcase
-		if choice == 'y'
-			search(p)
-		elsif choice == 'n'
-			return
+	def self.page_search(p) # page_search text for a term
+		puts 'Yo. Enter search term: '
+		term = gets.chomp.downcase
+		count_results = p.downcase.scan(term).count
+		result_found = true
+		result_found = false if count_results == 0
+
+		if result_found
+			print "Result found. Occurrences: #{count_results}\n"
+			print 'Search again? y/N: '
+			choice = gets.chomp.downcase
+			if choice == 'y'
+				page_search(p)
+			elsif choice == 'n'
+				return
+			else
+				puts 'Character unrecognised.'
+				page_search(p)
+			end
+
 		else
-			puts 'Character unrecognised.'
-			search(p)
+			print 'No result found. Search for another term? y/N: '
+			choice = gets.chomp.downcase
+			if choice == 'y'
+				page_search(p)
+			elsif choice == 'n'
+				return
+			else
+				puts 'Character unrecognised.'
+				page_search(p)
+			end
 		end
+	end
 
-	else
-		print 'No result found. Search for another term? y/N: '
+	# specify webpage to grab... auto append prefixes
+	def self.grab_page(p, options = {})
+		needs_http = false
+		needs_http = true if p.downcase.scan(/http:\/\//).count == 0
+		needs_www = false
+		needs_www = true if p.downcase.scan(/www./).count == 0
+
+		p.insert(0,'www.') if needs_www
+		p.insert(0,'http://') if needs_http
+		grabbed = Nokogiri::HTML(open(p))
+		
+		grabbed.text if options[:text]
+	end
+
+	# get input off user...
+	def self.get_input_page
+		'Hi there! Enter page to read: '.each_char do |c|
+			putc c
+			sleep 0.025
+			STDOUT.flush
+		end
+		page = gets.chomp
+
+		'Do you want to store this whole page as text? y/N: '
+		.each_char do |c|
+			putc c
+			sleep 0.025
+			STDOUT.flush
+		end
 		choice = gets.chomp.downcase
-		if choice == 'y'
-			search(p)
-		elsif choice == 'n'
-			return
+
+		if choice ==  'y'
+			puts "Storing webpage as text..."
+			grab_page(page, text: true)
 		else
-			puts 'Character unrecognised.'
-			search(p)
+			grab_page(page)
+			puts 'Page grabbed.'
 		end
 	end
-end
 
-# specify webpage to grab... auto append prefixes
-def grab_page(p, options = {})
-	needs_http = false
-	needs_http = true if p.downcase.scan(/http:\/\//).count == 0
-	needs_www = false
-	needs_www = true if p.downcase.scan(/www./).count == 0
-
-	p.insert(0,'www.') if needs_www
-	p.insert(0,'http://') if needs_http
-	grabbed = Nokogiri::HTML(open(p))
-	
-	grabbed.text if options[:text]
-end
-
-# get input off user...
-def get_input
-	'Hi there! Enter page to read: '.each_char do |c|
-		putc c
-		sleep 0.025
-		STDOUT.flush
-	end
-	page = gets.chomp
-
-	'Do you want to store this whole page as text? y/N: '
-	.each_char do |c|
-		putc c
-		sleep 0.025
-		STDOUT.flush
-	end
-	choice = gets.chomp.downcase
-
-	if choice ==  'y'
-		puts "Storing webpage as text..."
-		grab_page(page, text: true)
-	else
-		grab_page(page)
+	# man
+	def self.help 
 	end
 end
 
-# man
-def help 
-end
-
-### MAIN ###
-get_input
-
+D0xWeb::get_input_page
 =begin DEPRECATED
 lipsum = Nokogiri::HTML(open("http://www.lipsum.com")) 
 
@@ -106,8 +108,8 @@ bbb para.gsub('/<>/','') # remove characters, replace with ''
 # sweet unintended effect... seems to be removing tag 
 # along with angle brackets
 
-# para is stored as a string, so let's search it...
-search(para)
+# para is stored as a string, so let's page_search it...
+page_search(para)
 =end
 
 
