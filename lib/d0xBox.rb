f343226c60e799183a914a5cb9f27ed367df0430
fewzy <bj.1@gmx.com>
2013-10-31 15:31:09 +0000

    Update d0xBox.rb

--- lib/d0xBox.rb
+++ lib/d0xBox.rb
@@ -2,6 +2,26 @@ require 'rubygems'
 require 'nokogiri'
 require_relative './d0xBox/d0xWeb' # Really? You call that >clean?<
 
-### MAIN ###
-D0xWeb::get_input_page
+class D0xClient
+  def initialize
+  end
+end
+
+def main
+  print <<stop
+     _ _____     ______  _____      
+    | |  _  |    | ___ \|  _  |     
+  __| | |/' |_  _| |_/ /| |/' |_  __
+ / _` |  /| \ \/ / ___ \|  /| \ \/ /
+| (_| \ |_/ />  <| |_/ /\ |_/ />  < 
+ \__,_|\___//_/\_\____/  \___//_/\_\ 0.1
+ 
+stop
+  
+  
+end
+
+
+
+
 
